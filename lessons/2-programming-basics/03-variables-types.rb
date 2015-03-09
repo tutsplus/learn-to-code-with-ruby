e448b30cba3ac068ba69fec19891e4641d82cf99
José Mota <jose@josemota.net>
2015-03-09 15:17:56 +0000

    Lesson 2.3.

---
+++ lessons/2-programming-basics/03-variables-types.rb
@@ -0,0 +1,12 @@
+# puts "Hello World".class
+# puts 3.class
+# puts 3.14.class
+# puts true.class
+# puts false.class
+# puts [ true, false, 3, "Jose Mota" ].class
+# puts nil.class
+# puts :symbol.class
+# puts "symbol"
+
+puts "symbol".object_id == "symbol".object_id
+puts :symbol.object_id == :symbol.object_id
