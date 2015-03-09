d3d972d6ceed4165e0cc88ff57a8ca0979dad44b
José Mota <jose@josemota.net>
2015-03-09 15:42:52 +0000

    Lesson 2.4.

---
+++ lessons/2-programming-basics/04-if-else-unless.rb
@@ -0,0 +1,10 @@
+# puts "What's the gender letter (M/F) ?"
+# gender = gets.chomp
+
+# if gender == "M"
+# puts "It's a boy!"
+# elsif gender == "F"
+#   puts "It's a girl!"
+# else
+#   puts "I don't know that gender."
+# end
